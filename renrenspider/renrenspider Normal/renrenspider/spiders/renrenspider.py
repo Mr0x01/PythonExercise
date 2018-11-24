0eb32b1d838245c12eb552423fc1eed2c52c9f02
WhoIAm <RunFasterThanAnyOtherJournalist@git.com>
2018-11-24 15:14:09 +0800

    更改了好友的获取方式，以往的翻页方式可能获取不全，现在改为手动拼接url翻页

--- renrenspider/renrenspider Normal/renrenspider/spiders/renrenspider.py
+++ renrenspider/renrenspider Normal/renrenspider/spiders/renrenspider.py
@@ -1,8 +1,13 @@
-import scrapy,logging,urllib,time
+import scrapy
+import logging
+import urllib
+import time
 from renrenspider.items import *
+
 
 class Renrenspider(scrapy.Spider):
     name = "renrenspider"
+
     def start_requests(self):
         return [
             scrapy.Request(
@@ -12,15 +17,16 @@ class Renrenspider(scrapy.Spider):
         ]
 
     def login_page(self, response):
-        lbskey = response.css("div.form-inline>input[name=lbskey]::attr(value)").extract_first()  #取出登陆需要的值
-        email = input("输入账号:")  #账号
-        password = input("输入密码:")  #密码
+        lbskey = response.css(
+            "div.form-inline>input[name=lbskey]::attr(value)").extract_first()  # 取出登陆需要的值
+        email = input("输入账号:")  # 账号
+        password = input("输入密码:")  # 密码
         my_req = scrapy.FormRequest(
             url="http://3g.renren.com/login.do?autoLogin=true&&fx=0",
             method="POST",
             headers={
                 "Referer": "http://3g.renren.com/home.do"
-                },
+            },
             callback=self.login_callback,
             formdata={
                 "origURL": r"/home.do",
@@ -34,13 +40,13 @@ class Renrenspider(scrapy.Spider):
                 "password": password
             },
             meta={
-                "dont_redirect":True,
-                "handle_httpstatus_list":[302]
+                "dont_redirect": True,
+                "handle_httpstatus_list": [302]
             }
-            )
+        )
         return my_req
-    
-    def login_callback(self,response):
+
+    def login_callback(self, response):
         cookies = {}
         if response.status == 302:
             logging.info("检测到302")
@@ -53,7 +59,7 @@ class Renrenspider(scrapy.Spider):
                 cookies=cookies,
                 callback=self.home_page,
                 meta={
-                    "cookies":cookies
+                    "cookies": cookies
                 }
             )]
         elif response.text.find("请输入验证码") > -1:
@@ -66,48 +72,56 @@ class Renrenspider(scrapy.Spider):
                 cookies=cookies,
                 callback=self.home_page,
                 meta={
-                    "cookies":cookies
+                    "cookies": cookies
                 }
             )]
 
-    def home_page(self,response):
+    def home_page(self, response):
         logging.info("主页")
-        atags = response.css("div.sec > a")
-        for atag in atags:
-            link = atag.css("::attr(href)").extract_first()
-            if link.find("friendlist.do")>-1:
-                return response.follow(
-                    url=link,
-                    cookies=response.meta["cookies"],
-                    callback=self.frinds_page,
-                    meta={"cookies":response.meta["cookies"]}
-                    )
-    
-    def frinds_page(self,response):
-        logging.info("好友页") 
+        page = 0
+        return response.follow(
+            url="http://3g.renren.com/friendlist.do?curpage=" + str(page),
+            cookies=response.meta["cookies"],
+            callback=self.frinds_page,
+            meta={"cookies": response.meta["cookies"], "page": page+1}
+        )
+
+    def frinds_page(self, response):
+        logging.info("好友页")
         friendlist = response.css("div.list div:not(.l)")
         # print(response.text)
-        for friend in friendlist:
-            friend_link = friend.css("a.p::attr(href)").extract_first()
-            friend_id = urllib.parse.parse_qs(friend_link.split("?")[1])["id"][0]
-            album_link = "http://3g.renren.com/album/wmyalbum.do?id={0}".format(friend_id)
-            logging.info("好友ID：{0}".format(friend_id))
+        if len(friendlist) > 0:
+            for friend in friendlist:
+                friend_link = friend.css("a.p::attr(href)").extract_first()
+                friend_id = urllib.parse.parse_qs(
+                    friend_link.split("?")[1])["id"][0]
+                album_link = "http://3g.renren.com/album/wmyalbum.do?id={0}".format(
+                    friend_id)
+                logging.info("好友ID：{0}".format(friend_id))
+                yield scrapy.Request(
+                    url=album_link,
+                    cookies=response.meta["cookies"],
+                    callback=self.albumlist_page,
+                    meta={"cookies": response.meta["cookies"]}
+                )
             yield scrapy.Request(
-                url=album_link,
-                cookies=response.meta["cookies"],
-                callback=self.albumlist_page,
-                meta={"cookies":response.meta["cookies"]}
-            )
-        next_page_link = response.css("div.l > a[title='下一页']::attr(href)").extract_first()
-        if next_page_link !=None:
-            yield scrapy.Request(
-                url=next_page_link,
+                url="http://3g.renren.com/friendlist.do?curpage=" + str(response.meta["page"]),
                 cookies=response.meta["cookies"],
                 callback=self.frinds_page,
-                meta={"cookies":response.meta["cookies"]}
+                    meta={
+                        "cookies": response.meta["cookies"], "page": response.meta["page"]+1}
             )
+        # next_page_link = response.css(
+        #     "div.l > a[title='下一页']::attr(href)").extract_first()
+        # if next_page_link != None:
+        #     yield scrapy.Request(
+        #         url=next_page_link,
+        #         cookies=response.meta["cookies"],
+        #         callback=self.frinds_page,
+        #         meta={"cookies": response.meta["cookies"]}
+        #     )
 
-    def albumlist_page(self,response):
+    def albumlist_page(self, response):
         logging.info("相册列表页")
         album_list = response.css("a.p")
         if len(album_list) < 1:
@@ -119,65 +133,67 @@ class Renrenspider(scrapy.Spider):
                     url=album_url,
                     cookies=response.meta["cookies"],
                     callback=self.each_album,
-                    meta={"cookies":response.meta["cookies"]}
+                    meta={"cookies": response.meta["cookies"]}
                 )
-            next_page_link = response.css("a[title='下一页']::attr(href)").extract_first()
+            next_page_link = response.css(
+                "a[title='下一页']::attr(href)").extract_first()
             if next_page_link != None:
                 yield scrapy.Request(
                     url=next_page_link,
                     cookies=response.meta["cookies"],
                     callback=self.albumlist_page,
-                    meta={"cookies":response.meta["cookies"]}
+                    meta={"cookies": response.meta["cookies"]}
                 )
 
-    def each_album(self,response):
+    def each_album(self, response):
         logging.info("相册页")
         img_list = response.css("div.list")[0].css("a")
-        
+
         for img in img_list:
             img_url = img.css("::attr(href)").extract_first()
             yield scrapy.Request(
                 url=img_url,
                 cookies=response.meta["cookies"],
                 callback=self.each_img,
-                meta={"cookies":response.meta["cookies"]}
+                meta={"cookies": response.meta["cookies"]}
             )
-        
-        next_page_link = response.css("a[title='下一页']::attr(href)").extract_first()
+
+        next_page_link = response.css(
+            "a[title='下一页']::attr(href)").extract_first()
         if next_page_link != None:
             yield scrapy.Request(
                 url=next_page_link,
                 cookies=response.meta["cookies"],
                 callback=self.each_album,
-                meta={"cookies":response.meta["cookies"],"dont_redirect":True}
+                meta={
+                    "cookies": response.meta["cookies"], "dont_redirect": True}
             )
 
-    def each_img(self,response):
+    def each_img(self, response):
         logging.info("相片详情")
         img_item = RenrenspiderItem()
-        friend_name = response.css("body > div.ssec > b > a::text").extract_first()
+        friend_name = response.css(
+            "body > div.ssec > b > a::text").extract_first()
         img_item["friend_name"] = friend_name
-        album_name = response.css("body > div:nth-child(14) > b::text").extract_first()
+        album_name = response.css(
+            "body > div:nth-child(14) > b::text").extract_first()
         img_item["album_name"] = album_name
         comment = ""
         try:
-            comment = response.css("body > div:nth-child(15) > p:nth-child(3)")[0].css("::text").extract()[1].strip()
-        except IndexError :
-            comment = response.css("body > div:nth-child(15) > p:nth-child(3) *::text").extract_first().strip()
+            comment = response.css(
+                "body > div:nth-child(15) > p:nth-child(3)")[0].css("::text").extract()[1].strip()
+        except IndexError:
+            comment = response.css(
+                "body > div:nth-child(15) > p:nth-child(3) *::text").extract_first().strip()
         img_item["img_comment"] = comment
-        datetime = response.css("body > div:nth-child(15) > p.time::text").extract_first()
+        datetime = response.css(
+            "body > div:nth-child(15) > p.time::text").extract_first()
         if datetime == None and datetime == "":
             img_item["img_date"] = time.time_ns()
         else:
-            img_item["img_date"] = datetime.replace("上传时间:","").strip()
+            img_item["img_date"] = datetime.replace("上传时间:", "").strip()
         for taga in response.css("a"):
             if taga.css("::text").extract_first() == "下载":
                 img_item["img_url"] = taga.css("::attr(href)").extract_first()
                 break
         yield img_item
-
-    
-
-            
-
-
